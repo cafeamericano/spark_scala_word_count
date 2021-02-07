a28bd8ee9176d47ed1c0d2196f0ee25256f0fcae
Development <development@matthews-air.lan>
2021-02-06 21:31:22 -0500

    Added dataset examples

--- wordCount/index.scala
+++ wordCount/index.scala
@@ -1,3 +1,3 @@
 val text = sc.textFile("input.txt")
-val counts = text.flatMap(line => line.split(" ")).map(word => (word,1));
+val counts = text.flatMap(line => line.split(" ")).map(word => (word,1)).reduceByKey(_+_);
 counts.saveAsTextFile("output")
